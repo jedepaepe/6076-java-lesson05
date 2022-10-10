2fc762aaab179c66d6e05b6337cbb81893bf76de
Jean-Michel Depaepe <jean-michel.depaepe@equans.com>
2022-10-10 16:24:03 +0200

    exercice

---
+++ src/main/java/eu/epfc/c6076/lesson05/ClassExercice.java
@@ -0,0 +1,12 @@
+package eu.epfc.c6076.lesson05;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ClassExercice extends Application {
+    @Override
+    public void start(Stage stage) {
+        stage.setTitle("my first JavaFX class");
+        stage.show();
+    }
+}
